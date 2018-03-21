86bfba2712ba6545689aa94e91109dbae14d53c7
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-03-21 13:22:39 -0400

    add Link to StockIcon to link to stock details page

--- src/components/StockIcon.jsx
+++ src/components/StockIcon.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const StockIcon = ({ symbol, stockData }) => (
-  <div className='stock-icon'>
-    <h3>{symbol}</h3>
-    {stockData ? 
-      <div className='stock-icon-data'>
-        <p>{stockData.quote.companyName}</p>
-        <p>${stockData.quote.latestPrice}</p>
-        <p>
-          <span>{stockData.quote.change} </span>
-          <span>{stockData.quote.changePercent * 100}%</span>
-        </p>
-      </div>
-    :
-      <div className='stock-icon-data'>
-        <p>Loading...</p>
-      </div>
-    }
-  </div>
+  <Link to={`/${symbol}`}>
+    <div className='stock-icon'>
+      <h3>{symbol}</h3>
+      {stockData ? 
+        <div className='stock-icon-data'>
+          <p>{stockData.quote.companyName}</p>
+          <p>${stockData.quote.latestPrice}</p>
+          <p>
+            <span>{stockData.quote.change} </span>
+            <span>{stockData.quote.changePercent * 100}%</span>
+          </p>
+        </div>
+      :
+        <div className='stock-icon-data'>
+          <p>Loading...</p>
+        </div>
+      }
+    </div>
+  </Link>
 )
 
 export default StockIcon;
