cac842253d2474a3826c602a4af159435795e604
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-03-21 19:20:45 -0400

    add logo to header, link to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../images/bear-head-brown.png';
 
 const Header = (props) => (
   <header>
-    <h1>stockr</h1>
+      <img className='logo' src={logo} alt='logo' />
+      <Link to='/'>
+      <h1>stockr</h1>
+      </Link>
   </header>
 )
 
