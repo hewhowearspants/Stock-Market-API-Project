dcf81ff54bfec944e3798544d479eb714683f50c
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-03-21 18:46:33 -0400

    destructure and restructure

--- src/components/StockIcon.jsx
+++ src/components/StockIcon.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const StockIcon = ({ symbol, stockData }) => (
+const StockIcon = ({ symbol, stockData }) => {
+  if (stockData) {
+    var { companyName, latestPrice, change, changePercent } = stockData.quote;
+  }
+  return (
   <Link to={`/${symbol}`}>
     <div className='stock-icon'>
       <h3>{symbol}</h3>
+      <i>{stockData && companyName}</i>
       {stockData ? 
         <div className='stock-icon-data'>
-          <p>{stockData.quote.companyName}</p>
-          <p>${stockData.quote.latestPrice.toFixed(2)}</p>
-          <p>
-            <span>{stockData.quote.change.toFixed(2)} </span>
-            <span>{(stockData.quote.changePercent * 100).toFixed(2)}%</span>
+          <p className='current-price'>${latestPrice.toFixed(2)}</p>
+          <p style={{color: change === 0 ? '#3C455C' : change > 0 ? 'green' : 'red'}}>
+            <span>{change.toFixed(2)} </span>
+            <span>{changePercent > 0 ? <i className="fas fa-caret-up"></i> : <i className="fas fa-caret-down"></i>}</span>
+            <span>{(Math.abs(changePercent) * 100).toFixed(2)}%</span>
           </p>
         </div>
       :
@@ -21,6 +26,7 @@ const StockIcon = ({ symbol, stockData }) => (
       }
     </div>
   </Link>
-)
+  )
+}
 
 export default StockIcon;
