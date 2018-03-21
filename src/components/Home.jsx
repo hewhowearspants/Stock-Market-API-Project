4430568b1a1f5b882e17fca2ab77b6cb867b65d1
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-03-21 13:03:54 -0400

    change Home component to render StockIcon components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 
+import StockIcon from './StockIcon'
+
 class Home extends Component {
 
   renderStocks = () => {
-    return this.props.stocks.map((stock, index) => {
-      return <p key={index}>{stock}</p>
+    return this.props.stocks.map((symbol, index) => {
+      return <StockIcon key={index} symbol={symbol} stockData={this.props.stockData[symbol]} />
     })
   }
 
   render() {
-    const { stocks } = this.props;
-
     return (
       <div className='home'>
         <p>Welcome Home</p>
