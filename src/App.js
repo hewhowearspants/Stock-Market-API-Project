aaca5f261b9295934c57ec9a141227b9164fb912
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-03-21 12:34:30 -0400

    set up initial state, Home route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import axios from 'axios';
 import './App.css';
 
+import Header from './components/Header';
+import Home from './components/Home';
+
 class App extends Component {
+  state = {
+    stocks: ['FB', 'AAPL', 'GOOG', 'AMZN', 'MDB'],
+    stockData: []
+  }
+
   render() {
+    const { stocks } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <main>
+            <Route exact path = '/'
+              render={() => (
+                <Home stocks={stocks} />
+              )}
+            />
+          </main>
+        </div>
+      </Router>
     );
   }
 }
