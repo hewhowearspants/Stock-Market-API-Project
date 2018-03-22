1084817351c065c058ac7ca31df425d3019edbec
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-03-22 14:53:28 -0400

    restructure stock details header

--- src/components/StockDetails.jsx
+++ src/components/StockDetails.jsx
@@ -37,21 +37,28 @@ class StockDetails extends Component {
     const { symbol, stockData } = this.props;
     if (stockData) {
       var { companyName, latestPrice, change, changePercent } = stockData.quote;
+      var { logo } = stockData;
     }
 
     return (
       <div className='stock-details'>
-        <h1>{symbol} {stockData && <i>({companyName})</i>}</h1>
+        <div className='stock-header'>
+          {stockData && <div className='stock-logo' style={{backgroundImage: `url(${logo.url})`}}></div>}
+          <h1 className='stock-symbol'>{symbol}</h1> 
+          {stockData && <i className='stock-company-name'>({companyName})</i>}
+          {stockData && 
+            <div className='current'>
+              <h2>${latestPrice.toFixed(2)}</h2>
+              <p style={{color: change === 0 ? '#3C455C' : change > 0 ? 'green' : 'red'}}>
+                <span>{change.toFixed(2)} </span>
+                <span>{change > 0 ? <i className="fas fa-caret-up"></i> : <i className="fas fa-caret-down"></i>}</span>
+                <span>{(Math.abs(changePercent) * 100).toFixed(2)}%</span>
+              </p>
+            </div>}
+        </div>
         {stockData ? 
         <div className='stock-details-data'>
-          <div className='current'>
-            <h2>${latestPrice.toFixed(2)}</h2>
-            <p style={{color: change === 0 ? '#3C455C' : change > 0 ? 'green' : 'red'}}>
-              <span>{change.toFixed(2)} </span>
-              <span>{changePercent > 0 ? <i className="fas fa-caret-up"></i> : <i className="fas fa-caret-down"></i>}</span>
-              <span>{(Math.abs(changePercent) * 100).toFixed(2)}%</span>
-            </p>
-          </div>
+          
           <div className='history'>
             <h2>7-Day History</h2>
             {this.renderHistory()}
