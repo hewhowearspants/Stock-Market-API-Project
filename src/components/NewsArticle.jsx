68f828b85b3461fdb41cf001791ae8603c659831
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-03-22 14:52:10 -0400

    add more details to news articles

--- src/components/NewsArticle.jsx
+++ src/components/NewsArticle.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import moment from 'moment';
 
 const NewsArticle = ({ article }) => {
-  const { url, headline, summary } = article;
+  const { url, headline, summary, datetime, source } = article;
+  let formattedTime = moment(datetime).fromNow();
+
   return (
     <article className='news-article'>
-      <a href={url}>{headline}</a>
-      <p>{summary}</p>
+      <a className='headline' href={url}>{headline}</a><span className='date'> {formattedTime}</span>
+      <p className='source'>Source: {source}</p>
+      {!summary.includes('No summary available') && <p className='summary'>{summary}</p>}
     </article>
   )
 }
