330adf2a5cd13a243a68930e22f04877113c8b77
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-03-22 14:54:52 -0400

    base caret up/down rendering on change instead of percent, for consistency

--- src/components/StockIcon.jsx
+++ src/components/StockIcon.jsx
@@ -15,7 +15,7 @@ const StockIcon = ({ symbol, stockData }) => {
           <p className='current-price'>${latestPrice.toFixed(2)}</p>
           <p style={{color: change === 0 ? '#3C455C' : change > 0 ? 'green' : 'red'}}>
             <span>{change.toFixed(2)} </span>
-            <span>{changePercent > 0 ? <i className="fas fa-caret-up"></i> : <i className="fas fa-caret-down"></i>}</span>
+            <span>{change > 0 ? <i className="fas fa-caret-up"></i> : <i className="fas fa-caret-down"></i>}</span>
             <span>{(Math.abs(changePercent) * 100).toFixed(2)}%</span>
           </p>
         </div>
