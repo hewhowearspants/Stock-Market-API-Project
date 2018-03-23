fe154950c566895221ea7ac9b41586c1fed36fed
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-03-23 16:51:16 -0400

    add error handling for when user inputs bad stock symbol

--- src/components/StockIconForm.jsx
+++ src/components/StockIconForm.jsx
@@ -1,24 +1,47 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class StockIconForm extends Component {
   state = {
-    symbol: ''
+    symbol: '',
+    placeholder: 'Enter Symbol'
   }
 
   handleInputChange = (e) => {
     this.setState({
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value.toUpperCase()
     })
   }
 
-  render() {
-    const { symbol } = this.state;
+  handleSubmit = async (e) => {
+    e.preventDefault();
     const { index } = this.props;
+    const { symbol } = this.state;
+    try {
+      const res = await axios.get(`https://api.iextrading.com/1.0/stock/${symbol}/logo`);
+      res && this.props.updateStocks(index, symbol.toUpperCase())
+    } catch(err) {
+      this.setState({
+        symbol: '',
+        placeholder: 'BAD SYMBOL'
+      })
+      setTimeout(() => {
+        this.setState({
+          placeholder: 'Enter Symbol'
+        })
+      }, 1000);
+    }
+    
+  }
+
+  render() {
+    const { symbol, placeholder } = this.state;
+
     return (
       <div className='stock-icon-form'>
-        <form onSubmit={() => this.props.updateStocks(index, symbol.toUpperCase())}>
-        <input onChange={this.handleInputChange} placeholder='Enter Symbol' value={symbol} name='symbol' maxLength='5'/>
-        <button onClick={() => this.props.updateStocks(index, symbol.toUpperCase())}>OK</button>
+        <form onSubmit={this.handleSubmit}>
+        <input onChange={this.handleInputChange} placeholder={placeholder} value={symbol} name='symbol' maxLength='5'/>
+        <button onClick={this.handleSubmit}>OK</button>
         </form>
       </div>
     )
