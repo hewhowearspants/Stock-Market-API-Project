3aa26497efc18e89942ef0b25a42ffec96abd96b
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-03-23 16:21:58 -0400

    import and render StockIconForm component if symbol is null

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
-import StockIcon from './StockIcon'
+import StockIcon from './StockIcon';
+import StockIconForm from './StockIconForm';
 
 class Home extends Component {
 
   renderStocks = () => {
     return this.props.stocks.map((symbol, index) => {
-      return <StockIcon key={index} symbol={symbol} stockData={this.props.stocksData[symbol]} />
+      if(symbol !== null) {
+        return <StockIcon key={index} index={index} symbol={symbol} stockData={this.props.stocksData[symbol]} updateStocks={this.props.updateStocks} />
+      } else {
+        return <StockIconForm key={index} index={index} updateStocks={this.props.updateStocks} />
+      }
     })
   }
 
