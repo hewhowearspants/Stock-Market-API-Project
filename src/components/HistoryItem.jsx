61a7e50c27d902b0bb4ff222a7a1377ff73c306b
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-03-24 18:19:06 -0400

    minor structural changes to history item component

--- src/components/HistoryItem.jsx
+++ src/components/HistoryItem.jsx
@@ -5,10 +5,10 @@ const HistoryItem = ({ data }) => {
   return (
     <div className='history-item'>
       <b className='history-item-label'>{label}</b>
-      <b>${close.toFixed(2)}</b>
-      <div>
-      <p style={{color: 'green'}}><i className="fas fa-caret-up"></i>{high.toFixed(2)}</p>
-      <p style={{color: 'red'}}><i className="fas fa-caret-down"></i>{low.toFixed(2)}</p>
+      <b className='closing-price'>${close.toFixed(2)}</b>
+      <div className='high-low-prices'>
+        <p style={{color: 'green'}}><i className="fas fa-caret-up"></i>{high.toFixed(2)}</p>
+        <p style={{color: 'red'}}><i className="fas fa-caret-down"></i>{low.toFixed(2)}</p>
       </div>
     </div>
   )
