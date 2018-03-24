2cc0cfd1ddeb56b95ba797a0e20fc1c578b12198
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-03-24 18:18:23 -0400

    change attribution wording in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import moment from 'moment';
 const Footer = () => (
   <footer>
     <p>©{moment().format('YYYY')} Ryan Edwards</p>
-    <div className='attribution'>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></div>
+    <div className='attribution'>Bear icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></div>
   </footer>
 )
 
